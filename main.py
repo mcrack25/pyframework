c0a56ab3ebf551e409529a510501515cf514f06a
mcrack25 <mcrack@yandex.ru>
2021-06-27 21:42:36 +0300

    Delete some to trash

--- main.py
+++ main.py
@@ -5,39 +5,28 @@ from packages.logger import Logger
 from packages.dirs import Dirs
 from packages.remover import Remover
 
-# Подключаем библиотеки проекта
+# ПОДКЛЮЧАЕМ ВСЕ БИБЛИОТЕКИ ПРОЕКТА
 # .......................
 
-# Вписываем название программы
+# ВПИСЫВАЕМ НАЗВАНИЕ ПРОГРАММЫ
 program_name = 'ProgramName v1.0'
 
-# Получаем все конфиги
+# ПОЛУЧАЕМ ВСЕ КОНФИГИ
 config_struct = Config('struct').getAll()
 config_app = Config('app').getAll()
 config_env = Config().getEnv()
 
-# Получаем нужные пути к папкам
+# ПОЛУЧАЕМ ВСЕ ПУТИ К ПАПКАМ
 dir_logs = Dirs().get('logs')
 
-# Получаем нужные пути к файлам
+# ПОЛУЧАЕМ ВСЕ ПУТИ К ФАЙЛАМ
 log_file = os.path.join(dir_logs, config_app['log_file'])
 
-# Удаляем ненужные файлы
+# УДАЛЯЕМ НЕНУЖНЫЕ, СТАРЫЕ ФАЙЛЫ
 del_files = [
     log_file,
 ]
 Remover().remove_files(del_files)
-
-# Создаём переменные для программы
-if config_env['login']:
-    login = config_env['login']
-else:
-    login = config_app['login']
-
-if config_env['password']:
-    password = config_env['password']
-else:
-    password = config_app['password']
 
 if(__name__ == "__main__"):
     # Создаём структуру папок для работы
@@ -46,11 +35,26 @@ if(__name__ == "__main__"):
     # Создаём объект логера
     logger = Logger(log_file)
 
-    # Логируем
+    # Логируем запуск
     logger.save_show('Программа запущена!!!')
+    # ****************** ТУТ ВЫПОЛНЯЕТСЯ ПРОГРАММА
 
-    # ТУТ ВЫПОЛНЯЕТСЯ ПРОГРАММА
+    # СОЗДАЁМ ПЕРЕМЕННЫЕ ДЛЯ ПРОГРАММЫ С ЗАЩИЩЁННЫМИ ДЛЯ РАЗРАБОТКИ ДАННЫМИ
+    if config_env['login']:
+        login = config_env['login']
+    else:
+        login = config_app['login']
+    if config_env['password']:
+        password = config_env['password']
+    else:
+        password = config_app['password']
 
+
+
+
+
+
+    # ****************** ТУТ ВЫПОЛНЯЕТСЯ ПРОГРАММА
     logger.save_show('Программа выполнена!!!')
     print('')
     print('***')
